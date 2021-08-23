3b5ca74423e54e9d428f46ae8efd4664c92d7dfa
Mubarak Awal <awalmubarak6@gmail.com>
2021-08-23 21:17:29 +0000

    add display stories in horizontal scroll

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import Header from '../components/Header';
+import Stories from '../components/Stories';
 
 function Homepage() {
 	return (
 		<div className="homepage-main">
 			<Header />
 			<div className="homepage-main-content">
-				<div className="columns homepage-content">
-					<div className="column is-two-thirds">Hello from home</div>
+				<div className="columns homepage-content-container">
+					<div className="column is-two-thirds homepage-content">
+						<Stories />
+					</div>
 					<aside className="column homepage-sidebar">Hello</aside>
 				</div>
 			</div>
