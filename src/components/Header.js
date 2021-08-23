eafa7df8c1d989a288bcb3f9d52edc0eb24ea957
Mubarak Awal <awalmubarak6@gmail.com>
2021-08-23 19:16:43 +0000

    add header actions

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,7 +11,27 @@ function Header() {
 				<div className="column header-searchbar">
 					<SearchBar />
 				</div>
-				<div className="column header-actions">Icons</div>
+				<div className="column header-actions">
+					<div className="header-actions-item">
+						<span className="icon">
+							<ion-icon name="home-sharp"></ion-icon>
+						</span>
+					</div>
+
+					<div className="header-actions-item">
+						<span className="icon">
+							<ion-icon name="heart-outline"></ion-icon>
+						</span>
+					</div>
+
+					<div className="header-actions-item">
+						<img
+							src="/profile-pic.jpeg"
+							className="header-user-profile"
+							alt="user profile pic"
+						/>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
