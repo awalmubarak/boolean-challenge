a4bfd5c73ee3d02714a30e4e13d4c17f505da448
Mubarak Awal <awalmubarak6@gmail.com>
2021-08-23 22:28:56 +0000

    add post likes and post details sections

--- src/components/Post.js
+++ src/components/Post.js
@@ -47,6 +47,24 @@ function Post() {
 						</span>
 					</div>
 				</div>
+
+				<div className="post-likes-container">
+					<img
+						src="/profile-pic.jpeg"
+						alt="liked"
+						className="post-likes-recent-image"
+					/>
+					<span className="post-details-username">angela.hue</span>
+					<span className="post-details-text">
+						&nbsp;and 6 others
+					</span>
+					<span className="post-details-text">&nbsp;liked this</span>
+				</div>
+
+				<div className="post-owner-container">
+					<span className="post-details-username">angela.hue</span>
+					<span className="post-details-text">&nbsp;liked this</span>
+				</div>
 			</div>
 			<div className="post-comment-container"></div>
 		</div>
