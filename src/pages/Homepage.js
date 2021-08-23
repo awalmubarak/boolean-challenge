b6e3ff956ac7592e4859b4959d4cad4b625bf679
Mubarak Awal <awalmubarak6@gmail.com>
2021-08-23 19:53:13 +0000

    add home page content sections

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -5,6 +5,12 @@ function Homepage() {
 	return (
 		<div className="homepage-main">
 			<Header />
+			<div className="homepage-main-content">
+				<div className="columns homepage-content">
+					<div className="column is-two-thirds">Hello from home</div>
+					<aside className="column homepage-sidebar">Hello</aside>
+				</div>
+			</div>
 		</div>
 	);
 }
