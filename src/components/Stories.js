5338a0c4f57fd6898831993babcbd2759cf97a38
Mubarak Awal <awalmubarak6@gmail.com>
2021-08-24 12:59:36 +0000

    add stories loading spinner

--- src/components/Stories.js
+++ src/components/Stories.js
@@ -4,19 +4,24 @@ import StoryItem from './StoryItem';
 function Stories() {
 	return (
 		<div className="stories-main">
-			<StoryItem isLoading />
-			<StoryItem isLoading />
-			<StoryItem isLoading />
-			<StoryItem />
-			<StoryItem />
-			<StoryItem />
-			<StoryItem />
-			<StoryItem />
-			<StoryItem />
-			<StoryItem />
-			<StoryItem />
-			<StoryItem />
-			<StoryItem />
+			<div className="stories-loader">
+				<img src="/loading-buffering.gif" alt="stories loading" />
+			</div>
+			<div className="stories-list">
+				<StoryItem isLoading />
+				<StoryItem isLoading />
+				<StoryItem isLoading />
+				<StoryItem />
+				<StoryItem />
+				<StoryItem />
+				<StoryItem />
+				<StoryItem />
+				<StoryItem />
+				<StoryItem />
+				<StoryItem />
+				<StoryItem />
+				<StoryItem />
+			</div>
 		</div>
 	);
 }
