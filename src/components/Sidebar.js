db83ad04df0c744451390346a26fffaf369f3681
Mubarak Awal <awalmubarak6@gmail.com>
2021-08-24 10:39:32 +0000

    add follow suggestion header

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import CurrentUser from './sidebarComponents/CurrentUser';
+import FollowSuggestions from './sidebarComponents/FollowSuggestions';
 
 function Sidebar() {
 	return (
 		<div>
 			<div className="sidebar-currentuser">
 				<CurrentUser />
+			</div>
+			<div className="sidebar-suggestions">
+				<FollowSuggestions />
 			</div>
 		</div>
 	);
---
+++ src/components/sidebarComponents/FollowSuggestions.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function FollowSuggestions() {
+	return (
+		<div className="follow-suggestions-main">
+			<div className="follow-suggestions-header">
+				<span className="follow-suggestions-header-text">
+					Suggestions for you
+				</span>
+				<button className="follow-suggestions-header-button">
+					See All
+				</button>
+			</div>
+		</div>
+	);
+}
+
+export default FollowSuggestions;
